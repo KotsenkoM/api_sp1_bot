fd144056ee663d9bfc1b204131f21df45d8e1400
KotsenkoM <maksimkcenk93@gmail.com>
2021-08-19 23:32:16 +0300

    first review

--- homework.py
+++ homework.py
@@ -2,6 +2,7 @@ import os
 import time
 import requests
 import telegram
+import logging
 from dotenv import load_dotenv
 
 
@@ -14,12 +15,15 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
 # и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token='TELEGRAM_TOKEN')
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    status = homework['status']
+    if status != 'approved':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,20 +31,27 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    current_timestamp = current_timestamp
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url, headers=headers, params=payload)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(CHAT_ID, message)
 
 
 def main():
+    logging.info('Бот запущен')
+    logging.debug('api_sp1_bot')
     current_timestamp = int(time.time())  # Начальное значение timestamp
 
     while True:
         try:
-            ...
+            homeworks = get_homeworks(current_timestamp)
+            homework = homeworks['homeworks'][0]
+            message = parse_homework_status(homework)
+            send_message(message)
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
@@ -49,4 +60,9 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='api_sp1_bot.log',
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+    )
     main()
