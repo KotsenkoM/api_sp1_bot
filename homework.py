56060c9fc5e47b1d3a59ad9f3b176d5109e3e296
KotsenkoM <maksimkcenk93@gmail.com>
2021-08-20 00:21:23 +0300

    first review

--- homework.py
+++ homework.py
@@ -10,12 +10,9 @@ load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+CHAT_ID = os.getenv('CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = telegram.Bot(token='TELEGRAM_TOKEN')
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
@@ -31,7 +28,6 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    current_timestamp = current_timestamp
     payload = {'from_date': current_timestamp}
     homework_statuses = requests.get(url, headers=headers, params=payload)
     return homework_statuses.json()
@@ -44,18 +40,21 @@ def send_message(message):
 def main():
     logging.info('Бот запущен')
     logging.debug('api_sp1_bot')
-    current_timestamp = int(time.time())  # Начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             homeworks = get_homeworks(current_timestamp)
+            if len(homeworks.get('homeworks')) == 0:
+                time.sleep(20 * 60)
+                continue
             homework = homeworks['homeworks'][0]
             message = parse_homework_status(homework)
             send_message(message)
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            time.sleep(20 * 60)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            send_message(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
 
 
